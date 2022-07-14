7841b8ed61d35af8558b4d77fb6ce3801fb5a783
Faranosh  Amini <faranosh.amini4@gmail.com>
2022-07-14 12:35:34 +0430

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import './style.css';
+
+function showTodo (todo){
+    const formList = document.querySelector('.form-list');
+    const newList = document.createElement('li');
+    newList.classList.add('newList');
+    newList.innerHTML = `
+    <input id="${todo.index}" class="check" type="checkbox"/>
+    <span class="text">${todo.description}</span>
+    <button class="delete"><img src="../images/dots.svg"></button>
+  `;
+  formList.append(newList);
+}
+
+const todoList = [];
+let id = 1;
+function add (text){
+    const todo = {
+        description: text,
+        completed: false,
+        index: id += 1,
+    };
+    todoList.push(todo);
+    showTodo(todo);
+}
+
+const main = document.querySelector('.form');
+main.addEventListener ('submit', (event) => {
+    event.preventDefault();
+    const input = document.querySelector('.action');
+    const text = input.value.trim();
+    if (text !==''){
+        add(text);
+        input.value = '';
+        input.focus();
+    }
+});
+
+const completed = document.querySelector('.newList');
+completed.addEventListener('click', () => {
+    completed.classList.toggle('complete');
+});
